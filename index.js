ccdfb4db050cbbf4a8bd92c15fd25abfc5262032
haytv <haytv@vng.com.vn>
2019-12-05 18:10:42 +0700

    update 1

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ var http = require('http');
 
 console.log("server is already start");
 
+const a = 0;
+
 http.createServer(function (req, res) {
-    res.writeHead(300, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    a ++;
+    res.writeHead(300, { 'Content-Type': 'text/html' });
+    res.end('Hello World! ' + a);
 }).listen(8083);
 
 console.log("server started");
