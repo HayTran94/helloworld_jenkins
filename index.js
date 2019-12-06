fc3fad0394b6e6dc32de58db505121d995be9a0f
haytv <haytv@vng.com.vn>
2019-12-06 15:39:01 +0700

    add log when request come

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ console.log("server is already start");
 let a = 0;
 
 http.createServer(function (req, res) {
-    a ++;
+    a++;
     res.writeHead(300, { 'Content-Type': 'text/html' });
     res.end('Hello World! ' + a);
+    console.log("a=", a);
 }).listen(8083);
 
 console.log("server started");
